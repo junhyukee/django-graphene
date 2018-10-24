52dea5493aa08cb366aff59067735c93decbb25e
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-10-24 15:04:09 -0700

    set up mutation, set up admin, set up db

--- test_graphene/schema.py
+++ test_graphene/schema.py
@@ -3,6 +3,7 @@ from graphene_django import DjangoObjectType
 import graphene
 from .models import PersonalPost
 
+
 class PersonalPostType(DjangoObjectType):
     """Describe which model we want to expose through GraphQL."""
     class Meta:
@@ -10,6 +11,7 @@ class PersonalPostType(DjangoObjectType):
 
         # Describe the data as a node in a graph for GraphQL
         interfaces = (graphene.relay.Node, )
+
 
 class Query(graphene.ObjectType):
     """Describe which records we want to show."""
@@ -24,4 +26,29 @@ class Query(graphene.ObjectType):
         else:
             return PersonalPost.objects.filter(user=user)
 
-schema = graphene.Schema(query=Query)
+
+class CreatePersonalPost(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        content = graphene.String()
+
+    personalpost = graphene.Field(PersonalPostType)
+    ok = graphene.Boolean()
+    status = graphene.String()
+
+    def mutate(self, info, title, content):
+        user = info.context.user
+
+        if user.is_anonymous:
+            return CreatePersonalPost(ok=False, status="Must be logged in!")
+        else:
+            new_post = PersonalPost(title=title, content=content, user=user)
+            new_post.save()
+            return CreatePersonalPost(personalpost=new_post, ok=True, status="ok")
+
+
+class Mutation(graphene.ObjectType):
+    create_personal_post = CreatePersonalPost.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
